60492641f28cd4f309561581eb1bc5efd2da1842
Yusuf <132759468+AltunbasYusuf@users.noreply.github.com>
2025-06-06 22:35:17 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         //Main sadece diğer classları yönetmek için olacak Single responsibility principles için
     Quick_Setup qs =new Quick_Setup();
-    System.out.println(qs.VehicleType());
-
-
-
-
+    Vehicle vehicle=qs.quickSetup();
     }
 }
