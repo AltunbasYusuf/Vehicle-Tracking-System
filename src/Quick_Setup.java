1a8c36753e7b23769995c5fb4dc94b01ed5edfd9
Yusuf <132759468+AltunbasYusuf@users.noreply.github.com>
2025-06-06 22:35:55 +0300

    Add files via upload

--- src/Quick_Setup.java
+++ src/Quick_Setup.java
@@ -5,25 +5,54 @@ public class Quick_Setup {
 
 
     //Araç ve motor özelliklerini kullanıcıdan alacak Quick Setup arayüzü
-    public static void quickSetup(){
+    public static Vehicle quickSetup(){
         System.out.println("Welcome to Quick Setup! Please choose the vehicle:\n" + "1.Car\n" + "2.Motorcycle\n" + "3.Truck");
         String vehicleType=VehicleType();
+        System.out.println("Please give the "+vehicleType+ " model:");
+        String model=model();
         System.out.println("Please give the vehicle's motor type (diesel,electric or hybrid):");
         String motorType=motorType();
         System.out.println("Please give the horsepower of vehicle's motor:");
         double horsePower=horsePower();
+        System.out.println("Please give the fuel type of vehicle's motor (Gasoline/Diesel/LPG/Electric) :");
+        String fuelType= fuelType();
         System.out.println("Please give the fuel consumption of vehicle's motor (L/100km or kWh/100km) :");
         double fuelConsumption=fuelConsumption();
         System.out.println("Please give the CO₂ Emission (g/km) of vehicle's motor:");
         double co2Emission=co2Emission();
+        System.out.println("Please give the vehicle's tire type(summer/winter):");
+        String tireType=tireType();
         Motor vehicleMotor=new Motor(motorType,horsePower,fuelConsumption,co2Emission);
-        Vehicle vehicle= new Vehicle(vehicleType,vehicleMotor);
+        Vehicle vehicle= new Vehicle(vehicleType,vehicleMotor,model,tireType,fuelType);
+        return vehicle;
     }
 
 
 
+    public static String model() {
+        String model = input.nextLine();
+    return model;
+    }
 
+    public static String fuelType(){
+        String fuelType=input.nextLine();
+        return fuelType;
+    }
 
+    public static String tireType() {
+        String tireType = input.nextLine();
+        if (tireType.equalsIgnoreCase("summer")){
+            tireType="summer";
+        }
+        else if (tireType.equalsIgnoreCase("winter")){
+            tireType="winter";
+        }
+        else {
+            System.out.println("Undefined type");
+            return tireType();
+        }
+        return tireType;
+    }
 
 
     public static double co2Emission(){
